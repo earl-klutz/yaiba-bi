4bfc696860d475498758b23077a56ccc48177120
gerusuraimu <gerusuraimu@mail.gerusuraimu.com>
2026-08-09 12:25:12 +0900

    colab_ui.pyの定数をconfig参照に切り替え

--- src/yaiba_bi/colab_ui.py
+++ src/yaiba_bi/colab_ui.py
@@ -2,11 +2,6 @@
 
 GoogleColab上で使用するディレクトリを定数で提供する
 
-Attributes:
-    BASE (str): ファイルIO系ディレクトリのベース
-    INPUT (str): アップロードしたファイルを格納するディレクトリ
-    INTERMEDIATE (str): 中間ファイルを一時格納するディレクトリ
-    OUTPUT (str): 処理結果のファイルを格納するディレクトリ
 """
 
 
@@ -14,12 +9,15 @@ import os
 import shutil
 from typing import Iterator
 
+from yaiba_bi.core import config
 
+
+# TODO: 参照切替で定数廃止(PR前にコメントごと削除)
 # ディレクトリ定数
-BASE = "YAIBA_data"
-INPUT = f"{BASE}/input"
-INTERMEDIATE = f"{BASE}/intermediate"
-OUTPUT = f"{BASE}/output"
+# BASE = "YAIBA_data"
+# INPUT = f"{BASE}/input"
+# INTERMEDIATE = f"{BASE}/intermediate"
+# OUTPUT = f"{BASE}/output"
 
 
 # 定数イテレータ
@@ -32,9 +30,9 @@ def __iters() -> Iterator[str]:
         INPUT -> INTERMEDIATE -> OUTPUT の順で値を返す
     """
 
-    yield INPUT
-    yield INTERMEDIATE
-    yield OUTPUT
+    yield config.paths.COLAB_INPUT
+    yield config.paths.COLAB_INTERMEDIATE
+    yield config.paths.COLAB_OUTPUT
 
 
 # ディレクトリ初期設定
@@ -57,4 +55,4 @@ def finalize() -> None:
 
     """
 
-    shutil.rmtree(BASE)
+    shutil.rmtree(config.paths.COLAB_BASE)
